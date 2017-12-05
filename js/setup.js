13a1ea0fa5027e3ff4b88f4c9d3f8938d0cfc8fc
Safe_Mode <safe2mode@gmail.com>
2017-12-05 22:47:54 +0300

    Валидация ввода имени персонажа

--- js/setup.js
+++ js/setup.js
@@ -127,3 +127,35 @@ setupClose.addEventListener('keydown', function (evt) {
     closePopup(setup);
   }
 });
+
+var userNameInput = setup.querySelector('.setup-user-name');
+
+var detectEdge = function () {
+  return (navigator.userAgent.search(/Edge/) > 0) ? true : false;
+};
+
+var onNameSubmit = function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+};
+
+userNameInput.addEventListener('invalid', onNameSubmit);
+
+if (detectEdge) {
+  userNameInput.addEventListener('input', function (evt) {
+    var target = evt.target;
+
+    if (target.value.length < 2) {
+      target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+    } else {
+      target.setCustomValidity('');
+    }
+  });
+}
