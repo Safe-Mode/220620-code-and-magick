7edf9732516bd168d3e1b6dc14e573375f9c6c40
Safe_Mode <safe2mode@gmail.com>
2017-12-14 12:54:50 +0300

    Реализовал механиз перетаскивания окна настроек

--- js/dialog.js
+++ js/dialog.js
@@ -22,6 +22,8 @@
   var closePopup = function (popup) {
     popup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
+    setup.style.top = '';
+    setup.style.left = '';
   };
 
   var onInputFocus = function (evt) {
@@ -81,4 +83,42 @@
       }
     });
   }
+
+  var dialogHandle = setup.querySelector('.setup-user-pic');
+
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 })();
