a902a847e29e0abb26dd8cfc48eb5a73202e1a09
Safe_Mode <safe2mode@gmail.com>
2017-12-18 12:30:51 +0300

    Добавил функции загрузки данных на сервер

--- js/backend.js
+++ js/backend.js
@@ -29,8 +29,31 @@
       xhr.send();
     },
 
-    save: function (data, onLoad, onError) {
+    save: function (data, url, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
 
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError('Неизвестный статус: ' + xhr.status + ' ' + xhr.statusText);
+        }
+      });
+
+      xhr.addEventListener('error', function () {
+        onError('Произошла ошибка соединения');
+      });
+
+      xhr.addEventListener('timeout', function () {
+        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+      });
+
+      xhr.timeout = 10000;
+
+      xhr.open('POST', url);
+      xhr.send();
     }
   };
 })();
